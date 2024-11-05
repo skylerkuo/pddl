7651994fa7d43a3053fb3a5d0059b2c0723a558b
skylerkuo <kuo10529@gmail.com>
2024-11-05 14:28:03 +0800

    Update pddl.py

--- pddl.py
+++ pddl.py
@@ -1,48 +1,26 @@
 import subprocess
 import os
 
-domain_file = 'your domain.pddl'
-problem_file = 'your problem.pddl'
+domain_file = 'C:/Users/User/Desktop/pddlfile/domain.pddl'
+problem_file = 'C:/Users/User/Desktop/pddlfile/problem2.pddl'
 
-#since pddl would not put the action "lock" after "insert" so need to use the following function
-
-def process_solution(solution):
-    actions = solution.strip().split('\n')
-    
-    reordered_actions = []
-    pending_installations = [] 
-    
-    for i, action in enumerate(actions):
-        if action.startswith("(insert"):
-            wire_name = action.split()[2]
-            
-            for j in range(i + 1, len(actions)):
-                if actions[j].startswith("(lock") and wire_name in actions[j]:
-                    reordered_actions.append(action) 
-                    reordered_actions.append(actions[j])  
-                    pending_installations.append(j) 
-                    break
-            else:
-                reordered_actions.append(action)
-        elif i not in pending_installations:
-            reordered_actions.append(action)
-    
-    return '\n'.join(reordered_actions)
-
-cmd = ['pyperplan', domain_file, problem_file]
+cmd = ['pyperplan', '-s', 'gbf', '-H', 'hadd', domain_file, problem_file]
 
 try:
     result = subprocess.run(cmd, capture_output=True, text=True)
 
-    solution_file = problem_file + '.soln'
-    
-    if os.path.exists(solution_file):
-        with open(solution_file, 'r') as f:
-            solution = f.read()
-            solution = process_solution(solution)
-            print(solution)
+    if result.returncode == 0:
+        print("success!")
+        solution_file = problem_file + '.soln'
+        
+        if os.path.exists(solution_file):
+            print(f"solution file location: {solution_file}")
+            with open(solution_file, 'r') as f:
+                solution = f.read()
+                print("task plan:")
+                print(solution)
+        else:
+            print("do not find the file")
 
-    
 except FileNotFoundError:
-    print("pyperplan uninstall")
-
+    print("pyperplan 未安裝或路徑未正確設定。")
